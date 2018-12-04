c939b2f3f5db58b8c45300343532b14bc015dd6f
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2018-12-04 18:40:01 +0100

    force pulling from https

--- update.sh
+++ update.sh
@@ -7,6 +7,9 @@ rm -Rf node_modules
 rm -f package-lock.json
 
 echo "[ameize-client] pull from git repository"
+# force origin to https to bypass ssh problems
+git remote remove origin
+git remote add origin https://github.com/ircam-jstools/ameize-client
 git pull origin master
 echo "[ameize-client] install"
 npm install
